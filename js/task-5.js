d7698c5673512d3573469c9fdc72ea3935887af0
alina.vinnik <alinavinnikhgfff@gmail.com>
2026-02-18 11:11:19 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector('body');
+const btnChangeColor = document.querySelector('.js-btn');
+const spanColor = document.querySelector('.color');
+btnChangeColor.addEventListener('click', changeColor);
+function changeColor() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
